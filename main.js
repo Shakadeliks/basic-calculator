8cc0c23437b719dc574a7716a8ac4de356d03bab
Shakadeliks <shakurxmatiwana@gmail.com>
2022-07-25 18:15:39 +0200

    deploy

--- main.js
+++ main.js
@@ -14,7 +14,7 @@
             this.delete = document.querySelector('#delete');
         },
         displayBtns: function () {
-            const values = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, '.', '%'];
+            const values = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, '.'];
             const btns = values.map(item =>
                 `
                     <button class="digit_btn" value=${item}>
